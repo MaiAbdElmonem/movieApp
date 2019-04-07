7a917ba909460e3c5db66b33fb9e5b0c92863adb
mai ahmed <maiahmed@192.168.220.128>
2019-04-07 15:08:20 -0700

    Initial Commit

---
+++ movieApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  movieApp
+//
+//  Created by mai ahmed on 4/7/19.
+//  Copyright © 2019 mai ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ movieApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  movieApp
+//
+//  Created by mai ahmed on 4/7/19.
+//  Copyright © 2019 mai ahmed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
